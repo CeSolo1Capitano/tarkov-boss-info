8803e5357aef4b9b3056719f61a466676a28826c
Luca <luca.lafalce@mashfrog.com>
2022-08-22 00:21:43 +0200

    fix

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const opts = {
     password: process.env.TWITCH_OAUTH_TOKEN
   },
   channels: [
-    'primo_danno'
+    'primo_danno',
+    'scrotobeggins'
   ],
   connection: {
     reconnect: true
@@ -54,15 +55,49 @@ function onMessageHandler (target, context, msg, self) {
 
 // Function called when the "dice" command is issued
 function checkBoss (name) {
+  var returnString = "";
     for (let index = 0; index < arrayInfo.maps.length; index++) {
-        const element = arrayInfo.maps[index];
-        const maplower = element.name.toLowerCase();
+        const elements = arrayInfo.maps[index];
+        const maplower = elements.name.toLowerCase();
 
-        
-        if (maplower.includes(name)) {
+        if (!maplower.includes(name)) {
+          
+
+          for (let index = 0; index < elements.bosses.length; index++) {
+            const bosses = elements.bosses[index];
+            var bossLower = bosses.name.toLowerCase();
+            name = name.toLowerCase();
+
+            if (bossLower.includes(name)) {
+                var nameMaps = [];
+                const spawnLoc = [];
+                const spawnPercent = (bosses.spawnChance * 100) + '%';
+
+                console.log(elements)
+                for (let index = 0; index < bosses.spawnLocations.length; index++) {
+                    spawnLoc.push(bosses.spawnLocations[index].name);
+                    
+                }
+                nameMaps.push(bosses.name + ' spawn maps: ' + elements.name + ', spawnChance: '+ spawnPercent + ' spawnLocations: '+ spawnLoc.join());
+
+                returnString = nameMaps.join()
+            } else if (bossLower.includes(name)) {
+              var nameMaps = elements.name;
+              const spawnLoc = [];
+              const spawnPercent = (bosses.spawnChance * 100) + '%'
+              for (let index = 0; index < bosses.spawnLocations.length; index++) {
+                  spawnLoc.push(bosses.spawnLocations[index].name);
+                  
+              }
+              returnString =  bosses.name + ' spawn maps: ' + nameMaps + ', spawnChance: '+ spawnPercent + ' spawnLocations: '+ spawnLoc.join();
+            }
+          }
+        } else {
           var infoPush = [];
-          for (let index = 0; index < element.bosses.length; index++) {
-            const bosses = element.bosses[index];
+          for (let index = 0; index < elements.bosses.length; index++) {
+            const bosses = elements.bosses[index];
+
+            const spawnPercent = (bosses.spawnChance * 100) + '%'
 
             const spawnLoc = [];
             for (let index = 0; index < bosses.spawnLocations.length; index++) {
@@ -70,33 +105,14 @@ function checkBoss (name) {
                 
             }
 
-            infoPush.push(bosses.name+ ' spawnChance: '+ bosses.spawnChance + ',  spawnLocations: '+ spawnLoc.join())
+            infoPush.push(bosses.name+ ' spawnChance: '+ spawnPercent + ',  spawnLocations: '+ spawnLoc.join())
             
           }
-          return infoPush.join();
-        } else {
-          for (let index = 0; index < element.bosses.length; index++) {
-            const bosses = element.bosses[index];
-            var bossLower = bosses.name.toLowerCase();
-            name = name.toLowerCase();
-
-            if (bossLower.includes(name)) {
-                var nameMaps = element.name;
-                const spawnLoc = [];
-                for (let index = 0; index < bosses.spawnLocations.length; index++) {
-                    spawnLoc.push(bosses.spawnLocations[index].name);
-                    
-                }
-                return bosses.name + ' spawn maps: ' + nameMaps + ', spawnChance: '+ bosses.spawnChance + ' spawnLocations: '+ spawnLoc.join();
-            }
-          }
+          returnString = infoPush.join();
         }
-
-        
-        
     }
 
-    return ''
+    return returnString;
 }
 
 // Called every time the bot connects to Twitch chat
